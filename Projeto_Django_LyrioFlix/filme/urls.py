b2eb6754271b6338bd3f9e011151388a72a32d45
filipelyrio <filipelyrio@gmail.com>
2022-08-06 01:29:29 -0300

    Primeira versão completa do site

--- Projeto_Django_LyrioFlix/filme/urls.py
+++ Projeto_Django_LyrioFlix/filme/urls.py
@@ -1,5 +1,5 @@
-from django.urls import path, include
-from .views import Homepage, Homefilmes, Detalhesfilme, Pesquisafilme
+from django.urls import path, reverse_lazy
+from .views import Homepage, Homefilmes, Detalhesfilme, Pesquisafilme, Paginaperfil, Criarconta
 from django.contrib.auth import views as auth_view
 
 app_name ='filme'
@@ -11,4 +11,9 @@ urlpatterns = [
     path('pesquisa/', Pesquisafilme.as_view(), name='pesquisafilme'),
     path('login/', auth_view.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_view.LogoutView.as_view(template_name='logout.html'), name='logout'),
+    path('editarperfil/<int:pk>', Paginaperfil.as_view(), name='editarperfil'),
+    path('criarconta/', Criarconta.as_view(), name='criarconta'),
+    path('mudarsenha/', auth_view.PasswordChangeView.as_view(template_name='editarperfil.html',
+                                                            success_url=reverse_lazy('filme:homefilmes')),
+                                                             name='mudarsenha'),
 ]
--- Projeto_Django_LyrioFlix/filme/views.py
+++ Projeto_Django_LyrioFlix/filme/views.py
@@ -1,12 +1,29 @@
-from django.shortcuts import render
-from .models import Filme
-from django.views.generic import TemplateView, ListView, DetailView
+from django.shortcuts import render, redirect, reverse
+from .models import Filme, Usuario
+from .forms import CriarContaForm, FormHomepage
+from django.views.generic import TemplateView, ListView, DetailView, FormView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 # Create your views here.
 
-class Homepage(TemplateView):
+class Homepage(FormView):
     template_name = 'homepage.html'
+    form_class = FormHomepage
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('filme:homefilmes')
+        else:
+            return super().get(request, *args, **kwargs)
+
+    def get_success_url(self):
+        email = self.request.POST.get('email')
+        usuarios = Usuario.objects.filter(email=email)
+        if usuarios:
+            return reverse('filme:login')
+        else:
+            return reverse('filme:criarconta')
 
 
 class Homefilmes(LoginRequiredMixin, ListView):
@@ -45,3 +62,22 @@ class Detalhesfilme(LoginRequiredMixin, DetailView):
         context['filmes_relacionados'] = filmes_relacionados
         return context
 
+class Paginaperfil(LoginRequiredMixin, UpdateView):
+    template_name = 'editarperfil.html'
+    model = Usuario
+    fields = ['first_name', 'last_name', 'email']
+
+    def get_success_url(self):
+        return reverse('filme:homefilmes')
+
+
+class Criarconta(FormView):
+    template_name = 'criarconta.html'
+    form_class = CriarContaForm
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('filme:login')
